64d9c32beb7944787a5191442751a0744297be19
llacza <llacza050699beca@gmail.com>
2021-05-30 00:17:57 -0500

    Agregar archivos de proyecto.

---
+++ ClaseASPMVC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaseASPMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult CreaVista1()
+        {
+            ViewBag.Message = "Creación de Vista Número 1";
+
+            return View();
+        }
+
+        public ActionResult CreaVista2()
+        {
+            ViewBag.Message = "Creación de Vista Número 2";
+
+            return View();
+        }
+    }
+}
